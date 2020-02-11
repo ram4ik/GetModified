75bd98c8896fa89f37d153de6b977ae37ca749b1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-11 15:52:00 +0200

    init project

---
+++ GetModified/MenuBar/MenuBar.swift
@@ -0,0 +1,80 @@
+//
+//  MenuBar.swift
+//  GetModified
+//
+//  Created by ramil on 11.02.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuBar: View {
+    @State private var selected = 0
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Button(action: {
+                    self.selected = 1
+                }) {
+                    Image(systemName: "rectangle.stack.person.crop")
+                        .foregroundColor(self.selected == 1 ? .black : .gray)
+                }
+                Text("Portfolio")
+                    .foregroundColor(self.selected == 1 ? .black : .gray)
+            }
+            Spacer()
+            VStack {
+                Button(action: {
+                    self.selected = 2
+                }) {
+                    Image(systemName: "chart.bar")
+                        .foregroundColor(self.selected == 2 ? .black : .gray)
+                }
+                Text("Prices")
+                    .foregroundColor(self.selected == 2 ? .black : .gray)
+            }
+            Spacer()
+            Button(action: {
+                self.selected = 0
+            }) {
+                VStack {
+                    Image(systemName: "arrow.right.arrow.left")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color("Green"))
+                        .cornerRadius(40)
+                }
+            }
+            
+            Spacer()
+            VStack {
+                Button(action: {
+                    self.selected = 3
+                }) {
+                    Image(systemName: "creditcard")
+                        .foregroundColor(self.selected == 3 ? .black : .gray)
+                }
+                Text("Card")
+                    .foregroundColor(self.selected == 3 ? .black : .gray)
+            }
+            Spacer()
+            VStack {
+                Button(action: {
+                    self.selected = 4
+                }) {
+                    Image(systemName: "gear")
+                        .foregroundColor(self.selected == 4 ? .black : .gray)
+                }
+                Text("Settings")
+                    .foregroundColor(self.selected == 4 ? .black : .gray)
+            }
+        }.padding(.horizontal)
+    }
+}
+
+struct MenuBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBar()
+    }
+}
